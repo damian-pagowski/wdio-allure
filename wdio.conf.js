a338c7f3e48e5d9951d5595e4a1bc255ad0826e2
Damian Pagowski <damianconsu@protonmail.com>
2022-03-10 19:07:37 +0000

    added chrome startup param

--- wdio.conf.js
+++ wdio.conf.js
@@ -55,7 +55,7 @@ exports.config = {
       maxInstances: 5,
       browserName: "chrome",
       "goog:chromeOptions": {
-        args: ["headless", "disable-gpu", "no-sandbox"],
+        args: ["headless", "disable-gpu", "no-sandbox", "disable-dev-shm-usage"],
       },
       acceptInsecureCerts: true,
       // If outputDir is provided WebdriverIO can capture driver session logs

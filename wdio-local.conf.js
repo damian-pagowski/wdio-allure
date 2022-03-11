02f328adcca3e161eb090b197729dabce56eae25
Damian Pagowski <damianconsu@protonmail.com>
2022-03-11 22:08:51 +0000

    additional chrome startup params

--- wdio-local.conf.js
+++ wdio-local.conf.js
@@ -20,12 +20,10 @@ exports.config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: [
-      './test/specs/**/*.js'
-  ],
+  specs: ["./test/specs/**/*.js"],
   // Patterns to exclude.
   exclude: [
-      // 'path/to/excluded/files'
+    // 'path/to/excluded/files'
   ],
   //
   // ============
@@ -49,20 +47,25 @@ exports.config = {
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
   // https://saucelabs.com/platform/platform-configurator
   //
-  capabilities: [{
-  
+  capabilities: [
+    {
       // maxInstances can get overwritten per capability. So if you have an in-house Selenium
       // grid with only 5 firefox instances available you can make sure that not more than
       // 5 instances get started at a time.
       maxInstances: 5,
       //
-      browserName: 'chrome',
-      acceptInsecureCerts: true
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["--disable-dev-shm-usage", "--disable-gpu", "--no-sandbox"],
+        // args: ['--headless', '--disable-gpu']
+      },
+      acceptInsecureCerts: true,
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
       // excludeDriverLogs: ['bugreport', 'server'],
-  }],
+    },
+  ],
   //
   // ===================
   // Test Configurations
@@ -70,7 +73,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: "info",
   //
   // Set specific log levels per logger
   // loggers:
@@ -94,7 +97,7 @@ exports.config = {
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'http://localhost',
+  baseUrl: "http://localhost",
   //
   // Default timeout for all waitFor* commands.
   waitforTimeout: 10000,
@@ -110,15 +113,15 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['chromedriver'],
-  
+  services: ["chromedriver"],
+
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
   // see also: https://webdriver.io/docs/frameworks
   //
   // Make sure you have the wdio adapter package for the specific framework installed
   // before running any tests.
-  framework: 'mocha',
+  framework: "mocha",
   //
   // The number of times to retry the entire specfile when it fails as a whole
   // specFileRetries: 1,
@@ -134,14 +137,12 @@ exports.config = {
   // see also: https://webdriver.io/docs/dot-reporter
   reporters: [["allure", { outputDir: "allure-results" }]],
 
-
-  
   //
   // Options to be passed to Mocha.
   // See the full list at http://mochajs.org/
   mochaOpts: {
-      ui: 'bdd',
-      timeout: 60000
+    ui: "bdd",
+    timeout: 60000,
   },
   //
   // =====
@@ -231,7 +232,6 @@ exports.config = {
   // afterTest: function(test, context, { error, result, duration, passed, retries }) {
   // },
 
-
   /**
    * Hook that gets executed after the suite has ended
    * @param {Object} suite suite details
@@ -275,10 +275,10 @@ exports.config = {
   // onComplete: function(exitCode, config, capabilities, results) {
   // },
   /**
-  * Gets executed when a refresh happens.
-  * @param {String} oldSessionId session ID of the old session
-  * @param {String} newSessionId session ID of the new session
-  */
+   * Gets executed when a refresh happens.
+   * @param {String} oldSessionId session ID of the old session
+   * @param {String} newSessionId session ID of the new session
+   */
   //onReload: function(oldSessionId, newSessionId) {
   //}
-}
+};
